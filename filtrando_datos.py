afc728d42dbdc073504cbd5998933b6b543a5202
julianbarr368 <barraganjulian368@gmail.com>
2022-06-13 11:36:37 -0500

    Se hace desafio de list comprehension and high order funtions

--- filtrando_datos.py
+++ filtrando_datos.py
@@ -75,11 +75,24 @@ DATA = [
 def run():
     all_python_devs = [worker['name'] for worker in DATA if worker['language'] == 'python'] #list comprehention
     all_platzi_workers = [worker['name'] for worker in DATA if worker['organization'] == 'Platzi'] #list comprehention
+    
+    all_python_devs_name = list(filter(lambda worker: worker['language'] == 'python', DATA)) # Me devuelve los diccionarios con los desarrolladores de python
+    all_python_devs_name = list(map(lambda worker: worker['name'], all_python_devs_name)) # se selecciona solo los nombres
+
+    all_platzi_workers_name = list(filter(lambda worker: worker['organization'] == 'Platzi', DATA))# Filter, maps
+    all_platzi_workers_name = list(map(lambda worker: worker['name'], all_platzi_workers_name))
+
+    adults_name = [worker['name'] for worker in DATA if worker['age'] > 18] # edad con list comprehensions
+    old_people_add = [worker | {'old': worker['age'] > 70} for worker in DATA] # old whith list comprehentios
+
+
+
     adults = list(filter(lambda worker: worker['age'] > 18 , DATA)) #High funtions -lambda
     adults = list(map(lambda worker: worker['name'], adults)) #High funtions -lambda, solo se toma el nombre
+    
     old_people = list(map(lambda worker: worker | {'old': worker['age'] > 70 }, DATA)) #arega una una key a DATA
 
-    for worker in old_people:
+    for worker in old_people_add:
         print(worker)
     
 
